afc25970ef0b27325102c0468cf906b7bf100354
Marshajv <Marshajv@gmatc.matc.edu>
2017-04-05 20:54:55 -0500

    Init Commit
    this is the first

---
+++ BowlingGame/Assets/BallRotation.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallRotation : MonoBehaviour {
+
+    public enum RotationVals
+        {
+            MouseXY = 0,
+            MouseX = 1,
+            MouseY = 2
+        }
+
+    public static bool canRotate;
+
+    public RotationVals axes = RotationVals.MouseXY;
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+    public float minimumHor = -45.0f;
+    public float maximumVert = 45.0f;
+    private float _rotationX = 0;
+    private float _rotationY = 0;
+
+    void Start()
+    {
+        canRotate = true; 
+     }
+    void Update()
+    {
+        if(canRotate)
+        MouseRotation();
+    }
+
+    public void MouseRotation()
+        {
+            if (axes == RotationVals.MouseX)
+            {
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+            }else if (axes == RotationVals.MouseY){
+                _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+                _rotationX = Mathf.Clamp(_rotationX, minimumHor, maximumVert);
+
+                //float rotationY = transform.localEulerAngles.y;
+                //transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+            }
+            else
+            {
+                _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+                _rotationX = Mathf.Clamp(_rotationX, minimumHor, maximumVert);
+
+                _rotationY -= Input.GetAxis("Mouse X") * sensitivityHor;
+                _rotationY = Mathf.Clamp(_rotationY, minimumHor, maximumVert);
+                //float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+                //float rotationY = transform.localEulerAngles.y + delta;
+                transform.localEulerAngles = new Vector3(_rotationX, -_rotationY, 0);
+            }
+        }
+    }
---
+++ BowlingGame/Assets/BowlingBallControl.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BowlingBallControl : MonoBehaviour {
+
+    public float maxForce;
+    public float curForce;
+    public float timer;
+
+    private int shotCount;
+
+    public bool hasRotated;
+    public bool canFill;
+    private bool canRoll;
+
+    public Rigidbody rb;
+
+    public Image powFill;
+
+    public GameObject rotArrow;
+    public GameObject powArrow;
+    public GameObject BowlingBall;
+
+    void OnEnable()
+    {
+        hasRotated = false;
+        powArrow.SetActive(false);
+    }
+
+    void Update() {
+        Bowl();
+        if (canFill == true) {
+            if (powFill.fillAmount < 1)
+            {
+                powFill.fillAmount += .1f * Time.deltaTime;
+
+            }
+            else if (powFill.fillAmount >= 1)
+            {
+                powFill.fillAmount = 0;
+            }
+        }
+    }
+    
+    public void Bowl()
+    {
+        if(Input.GetButtonDown("Fire1") && !hasRotated)
+        {
+            shotCount++;
+            BallRotation.canRotate = false;
+            //rb.AddForce(transform.forward * currentForce);
+            //rotArrow.SetActive(false);
+            hasRotated = true;
+
+            if (Input.GetButtonDown("Fire1")&& shotCount == 1)
+            {
+                powArrow.SetActive(true);
+                shotCount = 0;
+                canFill = true;
+                canRoll = true;
+
+                if(canRoll== true)
+                {
+                    
+                }
+                
+                //rb.AddForce(transform.forward * curForce);
+                //powFill.fillAmount = curForce / maxForce;
+                //if(powFill.fillAmount <1 ){
+                //    powFill.fillAmount += .1f * Time.deltaTime;
+                         
+                //}
+                //else if(powFill.fillAmount >= 1){
+                //    powFill.fillAmount = 0;
+                }
+            }
+        }
+}
+
+
